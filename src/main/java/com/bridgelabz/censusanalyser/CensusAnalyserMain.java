818a623b28e3e2040a9ac0742c3c1b6925c5bb0b
sushant2931 <sushantlad8888@gmail.com>
2021-09-29 12:47:00 +0530

    ADDED : Welcome On Indian States Census Analyser Program

---
+++ src/main/java/com/bridgelabz/censusanalyser/CensusAnalyserMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.censusanalyser;
+
+public class CensusAnalyserMain {
+    public static void main(String[] args) {
+        System.out.println(".....Welcome In Indian States  Census Analyser Program...... ");
+    }
+}
